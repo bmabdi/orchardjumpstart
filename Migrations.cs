0a6c781b8e4ed8fed806cb8f47450dd7a63ffa87
bmabdi <basheabdi@gmail.com>
2018-04-22 17:24:10 -0400

    Add files via upload
    all the jumpstart files

---
+++ Migrations.cs
@@ -0,0 +1,30 @@
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Common.Models;
+using Orchard.Data.Migration;
+using Orchard.LearnOrchard.FeaturedProduct.Models;
+using Orchard.Widgets.Models;
+
+namespace Orchard.LearnOrchard.FeaturedProduct {
+    public class Migrations : DataMigrationImpl {
+
+        public int Create()
+        {
+            ContentDefinitionManager.AlterTypeDefinition(
+              "FeaturedProductWidget", cfg => cfg
+                .WithSetting("Stereotype", "Widget")
+                .WithPart(typeof(FeaturedProductPart).Name)
+                .WithPart(typeof(CommonPart).Name)
+                .WithPart(typeof(WidgetPart).Name));
+            return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            SchemaBuilder.CreateTable(typeof(FeaturedProductPartRecord).Name,
+              table => table
+                .ContentPartRecord()
+                .Column<bool>("IsOnSale"));
+            return 2;
+        }
+    }
+}
---
+++ ResourceManifest.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.UI.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orchard.LearnOrchard.FeaturedProduct
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineStyle("FeaturedProduct").SetUrl("FeaturedProduct.css");
+        }
+    }
+}
